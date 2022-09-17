c44ab52627a7f54b8243aa648445047b0fad3067
Ravindra Chaugule <ravindra.chaugule@gmail.com>
2022-09-17 14:12:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-(function () {
-  if (
-    !"mediaDevices" in navigator ||
-    !"getUserMedia" in navigator.mediaDevices
-  ) {
-    alert("Camera API is not available in your browser");
-    return;
-  }
+  // Check navigator support
+const hasMediaSupport = () => {
+  return ("mediaDevices" in navigator || "getUserMedia" in navigator.mediaDevices) ? true : false
+}
 
-  // get page elements
-  const video = document.querySelector("#video");
-  const btnPlay = document.querySelector("#btnPlay");
-  const btnPause = document.querySelector("#btnPause");
-  const btnScreenshot = document.querySelector("#btnScreenshot");
-  const btnChangeCamera = document.querySelector("#btnChangeCamera");
-  const screenshotsContainer = document.querySelector("#screenshots");
-  const canvas = document.querySelector("#canvas");
-  const devicesSelect = document.querySelector("#devicesSelect");
+if (hasMediaSupport()) {
+  // Store elements
+  const video = document.querySelector("#video-stream");
+  const takeScreenshot = document.querySelector("#takeScreenshot");
+  const switchCamera = document.querySelector("#switchCamera");
+  const pictures = document.querySelector("#pictures");
+  const canvasImageStore = document.querySelector("#canvasImageStore");
 
-  // video constraints
+  // Use selfie
+  let isSelfie = false;
+
+  // Current video stream
+  let videoStream;
+  
+
+  // Video config
   const constraints = {
     video: {
       width: {
@@ -33,66 +34,35 @@
     },
   };
 
-  // use front face camera
-  let useFrontCamera = true;
-
-  // current video stream
-  let videoStream;
-
-  // handle events
-  // play
-  btnPlay.addEventListener("click", function () {
-    video.play();
-    btnPlay.classList.add("is-hidden");
-    btnPause.classList.remove("is-hidden");
-  });
-
-  // pause
-  btnPause.addEventListener("click", function () {
-    video.pause();
-    btnPause.classList.add("is-hidden");
-    btnPlay.classList.remove("is-hidden");
-  });
-
-  // take screenshot
-  btnScreenshot.addEventListener("click", function () {
+  // Capture Image
+  takeScreenshot.addEventListener("click", function () {
     const img = document.createElement("img");
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext("2d").drawImage(video, 0, 0);
-    img.src = canvas.toDataURL("image/png");
-    screenshotsContainer.prepend(img);
+    canvasImageStore.width = video.videoWidth;
+    canvasImageStore.height = video.videoHeight;
+    canvasImageStore.getContext("2d").drawImage(video, 0, 0);
+    img.src = canvasImageStore.toDataURL("image/png");
+    pictures.prepend(img);
   });
 
-  // switch camera
-  btnChangeCamera.addEventListener("click", function () {
-    useFrontCamera = !useFrontCamera;
-
-    initializeCamera();
+  // Switch camera
+  switchCamera.addEventListener("click", function () {
+    isSelfie = !isSelfie;
+    initCamera();
   });
 
-  // stop video stream
-  function stopVideoStream() {
-    if (videoStream) {
-      videoStream.getTracks().forEach((track) => {
-        track.stop();
-      });
-    }
-  }
-
-  // initialize
-  async function initializeCamera() {
-    stopVideoStream();
-    constraints.video.facingMode = useFrontCamera ? "user" : "environment";
+  // Main function to init camera
+  async function initCamera() {
+    constraints.video.facingMode = isSelfie ? "user" : "environment";
 
     try {
       videoStream = await navigator.mediaDevices.getUserMedia(constraints);
-      alert('i m here')
       video.srcObject = videoStream;
     } catch (err) {
-      alert("Could not access the camera");
+      alert("Something went wrong. Check camera settings");
     }
   }
+  initCamera();
 
-  initializeCamera();
-})();
+} else {
+  alert("Something went wrong. Check camera settings");
+}
